69f33dddff91037b4b09920bf7f4994091cc0579
Ranita Saha <saharanitaa1230@gmail.com>
2022-02-17 12:33:47 +0530

    [feat]:completed homepage

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-const menuBar=document.querySelector("#menu-bar");
-const navBar=document.querySelector(".navbar");
-const slides=document.querySelectorAll(".slide-container");
-const prevSlide=document.querySelector("#prev");
-const nextSlide=document.querySelector("#next");
+const menuBar = document.querySelector("#menu-bar");
+const navBar = document.querySelector(".navbar");
+const slides = document.querySelectorAll(".slide-container");
+const prevSlide = document.querySelector("#prev");
+const nextSlide = document.querySelector("#next");
 
+menuBar.addEventListener("click", () => {
+  menuBar.classList.toggle("fa-times");
+  navBar.classList.toggle("active");
+});
 
-menuBar.addEventListener("click",()=>{
-    menuBar.classList.toggle("fa-times");
-    navBar.classList.toggle("active");
-})
+document.addEventListener("scroll", () => {
+  menuBar.classList.remove("fa-times");
+  navBar.classList.remove("active");
+});
 
-document.addEventListener("scroll",()=>{
-    menuBar.classList.remove("fa-times");
-    navBar.classList.remove("active");
-})
+let index = 0;
+nextSlide.addEventListener("click", nextSlider);
+prevSlide.addEventListener("click", prevSlider);
 
-let index=0;
-nextSlide.addEventListener("click",nextSlider);
-prevSlide.addEventListener("click",prevSlider);
-
-function nextSlider(){
-    slides[index].classList.remove('active');
-    index=(index+1)% slides.length;
-    slides[index].classList.add('active');
+function nextSlider() {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
 }
 
-function prevSlider(){
-    slides[index].classList.remove('active');
-    index=(index-1)% slides.length;
-    slides[index].classList.add('active');
+function prevSlider() {
+  slides[index].classList.remove("active");
+  index = (index - 1) % slides.length;
+  slides[index].classList.add("active");
 }
